b54a66ece64717acad63eb033ad88d0ccf4772a4
David Solis <david@soliskit.com>
2017-01-23 16:19:24 -0800

    remove unnecessary default declaration

--- db/migrate/20170114003000_create_floorplans.rb
+++ db/migrate/20170114003000_create_floorplans.rb
@@ -2,7 +2,7 @@ class CreateFloorplans < ActiveRecord::Migration[5.0]
   def change
     create_table :floorplans do |t|
       t.integer :layout_id
-      t.jsonb :windows, default: '{}'
+      t.jsonb :windows
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -30,9 +30,9 @@ ActiveRecord::Schema.define(version: 20170114005620) do
 
   create_table "floorplans", force: :cascade do |t|
     t.integer  "layout_id"
-    t.jsonb    "windows",    default: "{}"
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
+    t.jsonb    "windows"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "listings", force: :cascade do |t|
