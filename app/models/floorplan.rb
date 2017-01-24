2de95cbc4a53ba671938b05a490f585765d04818
David Solis <david@soliskit.com>
2017-01-23 18:03:41 -0800

    define studio windows

--- app/models/floorplan.rb
+++ app/models/floorplan.rb
@@ -3,6 +3,24 @@ class Floorplan < ApplicationRecord
   serialize :windows, HashSerializer
   store_accessor :windows, :left_window, :middle_window, :right_window
 
+  def studio_windows
+    output = ""
+
+    if windows.blank?
+      output = "❔❔❔"
+    end
+
+    windows.each do |window, position|
+      if position == "0"
+        output.concat("⬜️")
+      elsif position == "1"
+        output.concat("🖼")
+      end
+    end
+
+    return output
+  end
+
   def layout_path
     "https://www.rentnema.com/img/floorplans/plan/#{layout_id}.jpg"
   end
