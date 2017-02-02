5d0df88f0fd50ea67ec670daf5a68f90638983d5
David Solis <david@soliskit.com>
2017-02-01 17:43:31 -0800

    Revert "test subdomain versioning"
    This reverts commit f91126a2c84946ab29f8ff402fe0e842a7e8d349.

--- app/controllers/api/v1/flats_controller.rb
+++
@@ -1,7 +0,0 @@
-class Api::V1::FlatsController < ApplicationController
-  respond_to :json
-
-  def index
-    @flats = Flat.all
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
-require 'api_constraints'
-
 Rails.application.routes.draw do
   root "flats#index"
 
@@ -10,11 +8,5 @@ Rails.application.routes.draw do
   end
 
   resources :floorplans
-
-  namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api'}, path: '/' do
-    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
-      resources :flats, :only =>[:index]
-    end
-  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
