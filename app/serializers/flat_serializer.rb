fc842ad33b2c353bcdf51923cafa105a5a95f07e
David Solis <david@soliskit.com>
2017-02-01 16:02:39 -0800

    nest floorplan, price, and windows

--- app/serializers/flat_serializer.rb
+++ app/serializers/flat_serializer.rb
@@ -1,13 +1,19 @@
 class FlatSerializer < ActiveModel::Serializer
-  belongs_to :floorplan, optional: true
-
-  attributes :bed, :bath, :stack, :floor, :sqft, :is_active, :price_updated_at, :listings
-
-  def listings
-    object.listings.all
-  end
+  attributes :bed, :bath, :stack, :floor, :sqft, :is_active, :windows, :latest_price, :price_updated_at, :layout_path
 
   def price_updated_at
     object.listings.last.created_at
+  end
+
+  def latest_price
+    object.listings.last.price
+  end
+
+  def windows
+    object.floorplan.studio_windows unless object.floorplan.nil?
+  end
+
+  def layout_path
+    object.floorplan.layout_path unless object.floorplan.nil?
   end
 end
