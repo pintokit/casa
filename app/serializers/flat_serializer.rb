66c1883229227da91bc84e7c70fc9e53e65f324c
David Solis <david@soliskit.com>
2017-02-01 16:08:50 -0800

    formatting

--- app/serializers/flat_serializer.rb
+++ app/serializers/flat_serializer.rb
@@ -1,16 +1,16 @@
 class FlatSerializer < ActiveModel::Serializer
   attributes :bed, :bath, :stack, :floor, :sqft, :is_active, :windows, :latest_price, :price_updated_at, :layout_path
 
-  def price_updated_at
-    object.listings.last.created_at
+  def windows
+    object.floorplan.studio_windows unless object.floorplan.nil?
   end
 
   def latest_price
     object.listings.last.price
   end
 
-  def windows
-    object.floorplan.studio_windows unless object.floorplan.nil?
+  def price_updated_at
+    object.listings.last.created_at
   end
 
   def layout_path
