a26206dd35cc35eeb7e3330d7dbce5b332bae963
David Solis <david@soliskit.com>
2017-02-13 17:05:30 -0800

    output all listings

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -5,7 +5,7 @@ class ListingsController < ApplicationController
   # GET /listings.json
   def index
     @flat = Flat.find(params[:flat_id])
-    @listings = @flat.listings.last(10)
+    @listings = @flat.listings
     week_range = "(DATE_TRUNC('week', (created_at::timestamptz - INTERVAL '6 day' - INTERVAL '0 second') AT TIME ZONE 'Etc/UTC') + INTERVAL '6 day' + INTERVAL '0 second') AT TIME ZONE 'Etc/UTC'"
 
     price_query = "SELECT #{week_range} AS created_at, listings.flat_id, avg(listings.price) AS avg_price FROM listings WHERE listings.flat_id = #{@flat.id} AND listings.price > 0 GROUP BY listings.flat_id, #{week_range} ORDER BY created_at ASC;"
