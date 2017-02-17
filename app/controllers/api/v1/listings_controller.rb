855b39bed207fc0cd3c233ce5e0b5767282fc0eb
David Solis <david@soliskit.com>
2017-02-17 15:31:24 -0800

    fix: request only listings for current flat

--- app/controllers/api/v1/listings_controller.rb
+++ app/controllers/api/v1/listings_controller.rb
@@ -1,9 +1,17 @@
 module Api::V1
   class ListingsController < ApiController
+    before_action :set_flat, only: :index
 
     # GET /v1/flats/1/listings
     def index
-      render json: Listing.all, each_serializer: ListingSerializer
+      render json: @flat.listings, each_serializer: ListingSerializer
     end
+
+    private
+      # Use callbacks to share common setup or constraints between actions.
+      def set_flat
+        @flat = Flat.find(params[:flat_id])
+      end
+
   end
 end
