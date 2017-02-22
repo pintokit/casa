de75ef88d35c1b9164794aed19f7d6baf87cb52e
David Solis <david@soliskit.com>
2017-02-21 16:41:50 -0800

    refactor value score method

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -6,34 +6,19 @@ class Flat < ApplicationRecord
     floor + stack
   end
 
-  def window_score
+  def value_score
+    current_price = listings.last.price
     window_count = 0
     unless floorplan.nil? || floorplan.windows.nil?
       floorplan.windows.each_value do |window|
         window_count += window.to_i
       end
     end
-    score = normalize(window_count, 2, 5, false)
-    return score
-  end
-
-  def floor_score
-    score = normalize(floor.to_i, 3, 37, false)
-    return score
-  end
-
-  def sqft_score
-    score = normalize(sqft, 463, 906, false)
-    return score
-  end
-
-  def price_score
-    current_price = listings.last.price
-    score = normalize(current_price, 2800, 4700, true)
-    return score
-  end
-
-  def value_score
-    return window_score + floor_score + sqft_score + price_score
+    
+    price_score = normalize(current_price, 2800, 4700, true)
+    window_score = normalize(window_count, 2, 5, false)
+    floor_score = normalize(floor.to_i, 3, 37, false)
+    sqft_score = normalize(sqft, 463, 906, false)
+    return price_score + window_score + floor_score + sqft_score
   end
 end
