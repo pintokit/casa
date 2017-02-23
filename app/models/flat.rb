ef82ad9832f788d1a83b0cba537ea6d1be73a526
David Solis <david@soliskit.com>
2017-02-22 16:38:33 -0800

    refactor window score to avg

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -8,17 +8,19 @@ class Flat < ApplicationRecord
 
   def value_score
     current_price = listings.last.price
-    window_count = 0
+    window_avg = 0
+    window_sum = 0
     unless floorplan.nil? || floorplan.windows.nil?
       floorplan.windows.each_value do |window|
-        window_count += window.to_i
+        window_sum += window.to_i
       end
+      window_avg = window_sum/floorplan.windows.count
     end
-    
+
     price_score = normalize(current_price, 2800, 4700, true)
-    window_score = normalize(window_count, 2, 5, false)
+    window_score = normalize(window_avg, 1, 4, false)
     floor_score = normalize(floor.to_i, 3, 37, false)
     sqft_score = normalize(sqft, 463, 906, false)
-    return price_score + window_score + floor_score + sqft_score
+    return (price_score * 3.5) + window_score + sqft_score + floor_score
   end
 end
