8c715a9e719561f17430eab782b1ef903e5b3453
David Solis <david@soliskit.com>
2017-03-05 19:57:53 -0800

    scrape jasper flats

--- app/jobs/json_listing_job.rb
+++ app/jobs/json_listing_job.rb
@@ -4,6 +4,8 @@ class JsonListingJob < ApplicationJob
   def request_flats(hirise, url, bed_type)
     if hirise == :nema
       url = URI.parse("#{url}#{bed_type}")
+    elsif hirise == :jasper
+      url = URI.parse(url)
     end
 
     response = Net::HTTP.start(url.host, use_ssl: true) do |http|
@@ -15,12 +17,14 @@ class JsonListingJob < ApplicationJob
       print 'Failed to pull listings because the remote server issued a redirect'
     when Net::HTTPSuccess
       data_json = JSON.parse response.body
+      units_json = data_json['units']
 
       if hirise == :nema
-        units_json = data_json['units']
         nema_flats(hirise, units_json, bed_type)
+      elsif hirise == :jasper
+        jasper_flats(hirise, units_json)
       end
-      
+
     else # failed to receive response code 200
       print "#{response.error!}"
     end
