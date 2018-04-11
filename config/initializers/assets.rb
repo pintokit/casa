cb9765d38bd750f80e32c064c604417633d4447a
David Solis <litlmoz@gmail.com>
2018-04-10 19:40:40 -0700

    remove yarn

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,11 +6,11 @@ Rails.application.config.assets.version = '1.0'
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
-Rails.application.config.assets.paths << Rails.root.join('node_modules')
+# Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
 Rails.application.config.assets.precompile << proc do |path|
   true if path =~ /.(eot|svg|ttf|woff|png)z/
-end 
+end
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
