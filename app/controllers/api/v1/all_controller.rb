2248fa09c81e8abe7f1c198613d0cbf24848a840
David Solis <litlmoz@gmail.com>
2018-07-19 16:10:58 -0700

    enable caching for API calls

--- app/controllers/api/v1/all_controller.rb
+++ app/controllers/api/v1/all_controller.rb
@@ -1,8 +1,9 @@
 module Api::V1
   class AllController < ApiController
 
-    # GET /v1/all/flats/:model_name
+    # GET /v1/all/:model_name
     def export
+      expires_in 1.hours, public: :true
       case params[:model_name]
       when 'flats'
         render json: Flat.all.sort_by(&:value_score).reverse!, each_serializer: AllFlatListingSerializer
